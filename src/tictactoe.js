8baa8221a9e3d7d5e8439ca666f656bc03d453e1
Sarah Ozatici <Sarah.Ozatici@ibm.com>
2020-11-27 11:59:29 -0500

    add fxn to check for vertical winner

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -10,7 +10,7 @@ class TicTacToe {
         this.playerO = playerO;
 
         this.nextTurn = this.playerX;
-
+        this.winner = '';
         
     }
 
@@ -36,6 +36,120 @@ class TicTacToe {
         players.push(this.playerX, this.playerO);
         return players;
     }
+
+      checkForWinnerVertical(game) {
+        if (
+          game.board[0][0] === 'X' &&
+          game.board[1][0] === 'X' &&
+          game.board[2][0] === 'X'
+        ) {
+          game.winner = 'X';
+          console.log("hiii", game.board )
+    
+          game.gameRunning = false;
+          return true;
+    
+        }
+        if (
+          game.board[0][1] === 'X' &&
+          game.board[1][1] === 'X' &&
+          game.board[2][1] === 'X'
+        ) {
+          game.winner = 'X';
+          game.gameRunning = false;
+          return true;
+    
+        }
+        if (
+          game.board[0][2] === 'X' &&
+          game.board[1][2] === 'X' &&
+          game.board[2][2] === 'X'
+        ) {
+          game.winner = 'X';
+          game.gameRunning = false;
+          return true;
+    
+        }
+        if (
+          game.board[0][0] === 'O' &&
+          game.board[1][0] === 'O' &&
+          game.board[2][0] === 'O'
+        ) {
+          game.winner = 'O';
+          game.gameRunning = false;
+          return true;
+    
+        }
+        if (
+          game.board[0][1] === 'O' &&
+          game.board[1][1] === 'O' &&
+          game.board[2][1] === 'O'
+        ) {
+          game.winner = 'O';
+          game.gameRunning = false;
+          return true;
+    
+        }
+        if (
+          game.board[0][2] === 'O' &&
+          game.board[1][2] === 'O' &&
+          game.board[2][2] === 'O'
+        ) {
+          game.winner = 'O';
+          game.gameRunning = false;
+          return true;
+    
+        }
+    
+        return false;
+      }
+    
+      checkForWinnerDiagonal(game) {
+        if (
+          game.board[0][0] === 'X' &&
+          game.board[1][1] === 'X' &&
+          game.board[2][2] === 'X'
+        ) {
+          game.winner = 'X';
+          game.gameRunning = false;
+          return true;
+    
+        }
+        if (
+          game.board[2][0] === 'X' &&
+          game.board[1][1] === 'X' &&
+          game.board[0][2] === 'X'
+        ) {
+          game.winner = 'X';
+          game.gameRunning = false;
+          return true;
+    
+        }
+        if (
+            game.board[0][0] === 'O' &&
+            game.board[1][1] === 'O' &&
+            game.board[2][2] === 'O'
+          ) {
+            game.winner = 'O';
+            game.gameRunning = false;
+            return true;
+    
+          }
+          if (
+            game.board[2][0] === 'O' &&
+            game.board[1][1] === 'O' &&
+            game.board[0][2] === 'O'
+          ) {
+            game.winner = 'O';
+            game.gameRunning = false;
+            return true;
+    
+          }
+    
+          return false;
+      }
+
+
 }
 
 module.exports = TicTacToe;
