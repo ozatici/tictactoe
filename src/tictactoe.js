55449cc25c251ea405a1037f5fe960abce328d32
Sarah Ozatici <Sarah.Ozatici@ibm.com>
2020-11-27 11:29:24 -0500

    board created and tested

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,9 +1,9 @@
 class TicTacToe {
     constructor () {
         this.board = [
-            [],
-            [],
-            [],
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', ''],
         ];
     }
 }
